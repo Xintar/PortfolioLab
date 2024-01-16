321ae058a15d44a59557a27aceedb1c5e34773db
andrzej <andszczepanski@gmail.com>
2024-01-16 14:13:00 +0100

    Landing page dinamic

--- PortfolioLab/GoodHandsApp/models.py
+++ PortfolioLab/GoodHandsApp/models.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
-    PermissionsMixin
+    PermissionsMixin, User
 )
 from django.db import models
 from django.utils import timezone
+
+from PortfolioLab import settings
 
 
 class CustomUserManager(BaseUserManager):
@@ -41,3 +43,82 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         return self.email
 
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=255,
+        verbose_name="Nazwa"
+    )
+
+
+class Institution(models.Model):
+    TYPE_CHOICES = (
+        (1, "fundacja"),
+        (2, "organizacja pozarządowa"),
+        (3, "zbiórka lokalna")
+    )
+
+    name = models.CharField(
+        max_length=255,
+        verbose_name="Nazwa"
+    )
+    description = models.TextField(
+        verbose_name="Opis"
+    )
+    type = models.FloatField(
+        choices=TYPE_CHOICES,
+        default=1,
+        verbose_name="Instytucja"
+    )
+    categories = models.ManyToManyField(
+        Category,
+        verbose_name="Kategoria"
+    )
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField(
+        verbose_name="Liczba worków"
+    )
+    categories = models.ManyToManyField(
+        Category,
+        verbose_name="Kategoria"
+    )
+    institution = models.ForeignKey(
+        Institution,
+        on_delete=models.CASCADE,
+        verbose_name="Instytucja"
+    )
+    address = models.CharField(
+        max_length=255,
+        verbose_name="Adres"
+    )
+    phone_number = models.CharField(
+        max_length=20,
+        verbose_name="Numer telefonu"
+    )
+    city = models.CharField(
+        max_length=255,
+        verbose_name="Miasto"
+    )
+    zip_code = models.CharField(
+        max_length=20,
+        verbose_name="Kod pocztowy"
+    )
+    pick_up_date = models.DateTimeField(
+        verbose_name="Data odbioru"
+    )
+    pick_up_time = models.DateTimeField(
+        verbose_name="Czas odbioru"
+    )
+    pick_up_comment = models.TextField(
+        verbose_name="Komentarz do odbioru"
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        null=True,
+        default=None,
+        on_delete=models.SET_NULL,
+        blank=True,
+        verbose_name="Darczyńca"
+    )
